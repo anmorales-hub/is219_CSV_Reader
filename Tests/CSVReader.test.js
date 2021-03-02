da040d1645e756980afcbd8031599079a16eebb0
anmorales-hub <moralesand2018@gmail.com>
2021-03-02 01:51:23 -0500

    Fix: attempt to fix travis build

--- Tests/CSVReader.test.js
+++ Tests/CSVReader.test.js
@@ -2,15 +2,15 @@ const City = require('../src/Models/City');
 const CSVReader = require("../src/Index.js");
 
 test('CSVReader creates output', () => {
-    let result = CSVReader.Read('data/WorldCities.csv', City);
+    let result = CSVReader.Read('Data/WorldCities.csv', City);
     expect(result).toBeInstanceOf(Array);
 });
 test('CSVReader creates proper length list', () => {
-    let result = CSVReader.Read('data/WorldCities.csv', City);
+    let result = CSVReader.Read('Data/WorldCities.csv', City);
     expect(result).toHaveLength(6);
 });
 test('CSVReader correct output model type', () => {
-    let result = CSVReader.Read('data/WorldCities.csv', City);
+    let result = CSVReader.Read('Data/WorldCities.csv', City);
     result.forEach(function (data) {
         expect(data).toBeInstanceOf(City);
     })
